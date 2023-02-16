e3f3da7f0ebdd850df46286d468785536c50f521
Otakar Hyps <otakar.hyps@gmail.com>
2023-02-16 16:26:06 -0300

    second

---
+++ MyStyleD/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Thu, 16 Feb 2023 16:06:58 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
